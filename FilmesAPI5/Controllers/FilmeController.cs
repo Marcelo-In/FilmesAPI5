f1aace04db7a07132f2b6873ff6cb76973a6904d
Marcelo <marceloinoue@hotmail.com>
2023-01-26 20:21:07 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI5/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme(Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
