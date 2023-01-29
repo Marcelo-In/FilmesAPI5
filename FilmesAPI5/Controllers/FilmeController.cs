620c7dd4462c828a38f6f3316598e1bf01e0035a
Marcelo <marceloinoue@hotmail.com>
2023-01-29 20:01:59 -0300

    Iniciando uso do banco de dados

--- FilmesAPI5/Controllers/FilmeController.cs
+++ FilmesAPI5/Controllers/FilmeController.cs
@@ -1,6 +1,7 @@
 ﻿using FilmesAPI5.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FilmesAPI5.Controllers
 {
@@ -9,11 +10,32 @@ namespace FilmesAPI5.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme(Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+
+            return NotFound();
         }
     }
 }
