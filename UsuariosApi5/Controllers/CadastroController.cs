49d95f43ce5c7e44484a6ef322362fedbfeb9470
Marcelo <marceloinoue@hotmail.com>
2023-02-07 15:24:12 -0300

    Criando usuário com Identity

--- UsuariosApi5/Controllers/CadastroController.cs
+++ UsuariosApi5/Controllers/CadastroController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosApi5.Data.Dtos;
+using UsuariosApi5.Services;
 
 namespace UsuariosApi5.Controllers
 {
@@ -7,10 +9,18 @@ namespace UsuariosApi5.Controllers
     [Route("[controller]")]
     public class CadastroController : ControllerBase
     {
+        private CadastroService _cadastroService;
+
+        public CadastroController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
 
         [HttpPost]
         public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
         {
+            Result resultado = _cadastroService.CadastraUsuario(createDto);
+            if (resultado.IsFailed) return StatusCode(500);
             return Ok();
         }
     }
